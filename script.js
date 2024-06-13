6ce755285e1b15cb8c0ec4103311a2218e298e35
icochet <iwancochet@gmail.com>
2024-06-13 22:33:25 +0200

    Responsive de la navbar avec ajout d'un menu hamburger

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", headerFixer);
 
 function headerFixer() {
-    if (window.scrollY > 10) {
-        header.classList.add("active");
-    } else {
-        header.classList.remove("active");
-    }
+  if (window.scrollY > 10) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
 }
+
+// Navigation responsive
+
+var hamburgerButton = document.querySelector(".hamburger_menu");
+var nav = document.querySelector("nav");
+
+hamburgerButton.addEventListener("click", () => {
+  hamburgerButton.classList.toggle("active");
+  nav.classList.toggle("active");
+});
