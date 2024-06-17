8597948139108d1c1ab2b56dccd6180d3efb0d3a
icochet <iwancochet@gmail.com>
2024-06-17 18:52:43 +0200

    Ajout d'une fonction pour centrer la navbar par rapport à l'écran

--- js/script.js
+++ js/script.js
@@ -1,16 +1,40 @@
 // Header
 
-const header = document.querySelector("header");
-
-window.addEventListener("scroll", headerFixer);
-
 function headerFixer() {
+  const header = document.querySelector("header");
+
   if (window.scrollY > 10) {
     header.classList.add("active");
   } else {
     header.classList.remove("active");
   }
 }
+
+window.addEventListener("scroll", headerFixer);
+
+function menuCenter() {
+  const left = document.querySelector('header .logo');
+  const right = document.querySelector('header .cv');
+  const nav = document.querySelector('header nav');
+  let leftWidth = 0;
+  let rightWidth = 0;
+
+  if (left) {
+    leftWidth = parseInt(window.getComputedStyle(left).width);
+  }
+
+  if (right) {
+    rightWidth = parseInt(window.getComputedStyle(right).width);
+  }
+
+  if (leftWidth < rightWidth) {
+    nav.style.paddingLeft = (rightWidth - leftWidth) + 'px';
+  } else {
+    nav.style.paddingRight = (leftWidth - rightWidth) + 'px';
+  }
+}
+
+menuCenter();
 
 // Navigation responsive
 
